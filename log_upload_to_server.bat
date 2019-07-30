c263a650506c521602e6657b4f8316e5d8e29749
efco-sam <samlee@efco.com.tw>
2019-07-30 15:47:04 +0800

    2019/07/30 週二

---
+++ log_upload_to_server.bat
@@ -0,0 +1,10 @@
+:: git config --global user.name "efco-sam"
+:: git config --global user.email "samlee@efco.com.tw"
+
+git pull
+git add .
+git commit -m "%date%"
+git push
+git log -p -1 -U1 --word-diff
+
+pause
